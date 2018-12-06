48fbc4b20fb7df6f34a3088f6a2b0d8aca1faad6
gaoan <gaoan@tigerbrokers.com>
2018-12-06 14:35:42 +0800

    mod: order id issue

--- tigeropen/trade/domain/order.py
+++ tigeropen/trade/domain/order.py
@@ -10,13 +10,14 @@ from enum import Enum
 ORDER_FIELDS_TO_IGNORE = {'type', '_status', 'contract'}
 
 ORDER_STATUS = Enum('OrderStatus', ('PENDING_NEW', 'NEW', 'HELD', 'PARTIALLY_FILLED', 'FILLED',
-                    'CANCELLED', 'PENDING_CANCEL', 'REJECTED', 'EXPIRED',))
+                                    'CANCELLED', 'PENDING_CANCEL', 'REJECTED', 'EXPIRED',))
 
 
 class Order(object):
-    __slots__ = ["id", "order_time", "reason", "trade_time", "contract", "action", "quantity", "filled", "_remaining",
-                 "avg_fill_price", "commission", "_status", "trail_stop_price", "limit_price", "aux_price",
-                 "trailing_percent", "percent_offset", "action", "order_type", "time_in_force"]
+    __slots__ = ["id", "order_id", "parent_id", "order_time", "reason", "trade_time", "contract", "action", "quantity",
+                 "filled", "_remaining", "avg_fill_price", "commission", "realized_pnl", "_status", "trail_stop_price",
+                 "limit_price", "aux_price", "trailing_percent", "percent_offset", "action", "order_type",
+                 "time_in_force", "outside_rth"]
 
     def __init__(self, account, contract, action, order_type, quantity, limit_price=None, aux_price=None,
                  trail_stop_price=None, trailing_percent=None, percent_offset=None, time_in_force=None,
@@ -30,7 +31,7 @@ class Order(object):
                   a negative sign indicates a sell
         @filled - how many shares of the order have been filled so far
         """
-        
+
         self.id = id
         self.order_id = order_id
         self.parent_id = parent_id
@@ -55,15 +56,15 @@ class Order(object):
         self.percent_offset = percent_offset
         self.order_time = None
         self.trade_time = None
-    
+
     def to_dict(self):
         dct = {name: getattr(self, name) for name in self.__slots__ if name not in ORDER_FIELDS_TO_IGNORE}
-        
+
         dct['contract'] = self.contract
         dct['status'] = self.status
-        
+
         return dct
-    
+
     @property
     def status(self):
         if not self.remaining:
@@ -72,33 +73,33 @@ class Order(object):
             return ORDER_STATUS.PARTIALLY_FILLED
         else:
             return self._status
-    
+
     @status.setter
     def status(self, status):
         self._status = status
-    
+
     @property
     def active(self):
         return self.status in [ORDER_STATUS.PENDING_NEW, ORDER_STATUS.NEW, ORDER_STATUS.PARTIALLY_FILLED,
-            ORDER_STATUS.HELD, ORDER_STATUS.PENDING_CANCEL]
-    
+                               ORDER_STATUS.HELD, ORDER_STATUS.PENDING_CANCEL]
+
     @property
     def remaining(self):
         return self.quantity - self.filled
-    
+
     @remaining.setter
     def remaining(self, value):
         """
         The setter of the remaining property
         """
         self._remaining = value
-    
+
     def __repr__(self):
         """
         String representation for this object.
         """
         return "Order(%s)" % self.to_dict().__repr__()
-    
+
     def __unicode__(self):
         """
         Unicode representation for this object.
