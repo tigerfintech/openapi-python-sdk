1148984106c0531f6c9186afaf920c63619e4b36
gaoan <gaoan@tigerbrokers.com>
2019-01-16 15:40:37 +0800

    add: fetch active orders

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = ['six', 'simplejson', 'delorean', 'pandas', 'python-dateutil'
 
 setup(
     name='tigeropen',
-    version='1.1.0',
+    version='1.1.1',
     description='TigerBrokers Open API',
     packages=find_packages(exclude=[]),
     author='TigerBrokers',
