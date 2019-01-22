e3728ac40e639f0ebfe8f041bb94ec72fb8cba25
xuqiang <xuqiang@xuqiangdeMacBook-Pro.local>
2019-01-22 12:09:36 +0800

    [tigeropen][tinyengine] add tick strategy

--- tigeropen/examples/tinyengine/strategy.py
+++ tigeropen/examples/tinyengine/strategy.py
@@ -1,4 +1,53 @@
+import numpy as np
 from tigeropen.common.consts import SecurityType, Currency
+
+
+class TickerTrendUtil:
+    def __init__(self):
+        self.price_dict = {}
+
+    def on_latest_price(self, symbol, latest_price):
+        """
+        093003 latest_price1
+        093006 latest_price2
+        093009 latest_price3
+
+        return latest_price1 > latest_price2 < latest_price3
+        """
+        curr_price = self.price_dict.get(symbol)
+        if curr_price:
+            if len(curr_price) == 1:
+                curr_price.append(latest_price)
+                return False
+            else:
+                first = curr_price.pop(0)
+                second = curr_price[0]
+
+                curr_price.append(latest_price)
+
+                return first > second < latest_price
+        else:
+            self.price_dict[symbol] = [latest_price]
+            return False
+
+
+class EMAUtil:
+    def __init__(self, size):
+        self.buf = np.zeros(size)
+        self.maxsize = size
+        self.head = 0
+        self.sum = 0
+
+    def get_osc(self):
+        return self.sum / self.maxsize
+
+    def on_new_value(self, value):
+        tmp = self.buf[self.head]
+        self.buf[self.head] = value
+        self.sum += value - tmp
+        self.head += 1
+        if self.head >= self.maxsize:
+            self.head -= self.maxsize
 
 
 class Strategy(object):
@@ -12,11 +61,16 @@ class Strategy(object):
         self.open_time = '093000'
         self.close_time = '150000'
         self.time_zone = 'Asia/Shanghai'
-        self.event_trigger = True
+        self.event_trigger = False
 
-        self.symbol_market_map = {'600029': {'security': SecurityType.STK, 'currency': Currency.CNH},
-                                  '600053': {'security': SecurityType.STK, 'currency': Currency.CNH},
-                                  '600604': {'security': SecurityType.STK, 'currency': Currency.CNH}}
+        self.symbol_market_map = {'600029': {'security': SecurityType.STK, 'per_trade': 100},
+                                  '600053': {'security': SecurityType.STK, 'per_trade': 100},
+                                  '600604': {'security': SecurityType.STK, 'per_trade': 100}}
+        self.symbol_set = set(self.symbol_market_map.keys())
+        self.tick_util = TickerTrendUtil()
+
+        self.short_min_ma = EMAUtil(10)
+        self.long_min_ma = EMAUtil(20)
 
     def on_ticker(self, symbol, items, hour_trading):
         """
@@ -25,6 +79,14 @@ class Strategy(object):
         if hour_trading:
             return
         print(symbol, items, hour_trading)
+        latest_price = dict(items).get('latest_price')
+        if symbol in self.symbol_set:
+            if self.tick_util.on_latest_price(symbol, latest_price):
+                print('============= place order =============')
+                contract = self.context.contract_map.get(symbol)
+                symbol_market = self.symbol_market_map[symbol]
+                account = self.context.asset_manager.keys()[0]
+                self.trade_client.create_order(account, contract, 'BUY', 'MKT', symbol_market['per_trade'], limit_price=latest_price)
 
     def on_minute_bar(self, data):
         """
@@ -32,7 +94,8 @@ class Strategy(object):
         stop when time <= end_time
         """
         print(data.current(list(self.symbol_market_map.keys()), ['open', 'high', 'low', 'close', 'volume', 'time']))
-
+        # history api is not recommended to use
+        # data cannot be filled in
         print(data.history('600053', ['open', 'high', 'low', 'close', 'volume', 'time'], 10, '1m'))
 
     def before_trading_start(self):
