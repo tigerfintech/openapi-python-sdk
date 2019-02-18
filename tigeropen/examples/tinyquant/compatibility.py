abcea3e7900f660b3d57cc477795e714263f33fc
Kai Su <sukai@tigerbrokers.com>
2019-02-18 10:59:51 +0800

    修改：     compatibility.py 	修改：     context.py

--- tigeropen/examples/tinyquant/compatibility.py
+++ tigeropen/examples/tinyquant/compatibility.py
@@ -1,43 +1,26 @@
 """
 provide compatibility for tiger quant platform strategies.
 """
-from .context import global_context, SecurityType, Currency
+from .context import global_context
 from logbook import Logger
 log = Logger(__name__)
 
-SECURITY_TYPE_MAP = {
-    SecurityType.STK.name: SecurityType.STK,  # 股票
-    SecurityType.OPT.name: SecurityType.OPT,  # 期权
-    SecurityType.WAR.name: SecurityType.WAR,  # 窝轮
-    SecurityType.IOPT.name: SecurityType.IOPT,  # 权证(牛熊证)
-    SecurityType.FUT.name: SecurityType.FUT,  # 期货
-    SecurityType.FOP.name: SecurityType.FOP,  # 期货期权
-    SecurityType.CASH.name: SecurityType.CASH,  # 外汇
-}
-CURRENCY_MAP = {
-    Currency.USD.name: Currency.USD,  # 美元
-    Currency.HKD.name: Currency.HKD,  # 港毕
-    Currency.CNH.name: Currency.CNH,  # 离岸人民币
-}
 
-
-def symbol(symbol_str, sec_type=SecurityType.STK.name, currency=Currency.USD.name, exchange=None):
+def symbol(symbol_str, currency='USD', security_type='STK', exchange=None):
     """
-
     :param symbol_str: 证券类型, 默认股票
-    :param sec_type: 非股票需要指定sec_type
+    :param security_type: 非股票需要指定security_type
     :param currency: 非美股symbol需要制定currency
     :param exchange:
     :return:
     """
-    global_context.subscribe(symbol_str, security_type=SECURITY_TYPE_MAP.get(sec_type.upper()), currency=CURRENCY_MAP.get(currency.upper()), exchange=exchange)
+    global_context.subscribe(symbol_str, currency=currency, security_type=security_type, exchange=exchange)
     return symbol_str
 
 
-def symbols(*args, sec_type=SecurityType.STK.name, currency=Currency.USD.name, exchange=None):
+def symbols(*args, currency='USD', security_type='STK', exchange=None):
     for sym in list(*args):
-        global_context.subscribe(sym, security_type=SECURITY_TYPE_MAP.get(sec_type.upper()),
-                                 currency=CURRENCY_MAP.get(currency.upper()), exchange=exchange)
+        global_context.subscribe(sym, currency=currency, security_type=security_type, exchange=exchange)
     return list(*args)
 
 
--- tigeropen/examples/tinyquant/context.py
+++ tigeropen/examples/tinyquant/context.py
@@ -1,5 +1,20 @@
 from tigeropen.common.consts import SecurityType, Currency
 
+SECURITY_TYPE_MAP = {
+    SecurityType.STK.name: SecurityType.STK,  # 股票
+    SecurityType.OPT.name: SecurityType.OPT,  # 期权
+    SecurityType.WAR.name: SecurityType.WAR,  # 窝轮
+    SecurityType.IOPT.name: SecurityType.IOPT,  # 权证(牛熊证)
+    SecurityType.FUT.name: SecurityType.FUT,  # 期货
+    SecurityType.FOP.name: SecurityType.FOP,  # 期货期权
+    SecurityType.CASH.name: SecurityType.CASH,  # 外汇
+}
+CURRENCY_MAP = {
+    Currency.USD.name: Currency.USD,  # 美元
+    Currency.HKD.name: Currency.HKD,  # 港毕
+    Currency.CNH.name: Currency.CNH,  # 离岸人民币
+}
+
 
 class Context:
     def __init__(self):
@@ -13,13 +28,13 @@ class Context:
         self.currency_map = {}
         self.subscribed_symbols = set()
 
-    def subscribe(self, symbols, security_type=SecurityType.STK, currency=Currency.USD, exchange=None):
+    def subscribe(self, symbols, currency='USD', security_type='STK', exchange=None):
         if not (isinstance(symbols, list) or isinstance(symbols, set)):
             symbols = [symbols]
         for symbol in symbols:
             self.subscribed_symbols.add(symbol)
-            self.security_type_map[symbol] = security_type
-            self.currency_map[symbol] = currency
+            self.security_type_map[symbol] = SECURITY_TYPE_MAP.get(security_type.upper())
+            self.currency_map[symbol] = CURRENCY_MAP.get(currency.upper())
 
 
 global_context = Context()
