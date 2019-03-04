d92fc97e66723a4601ab3477f3d82405b6b7fc92
xuqiang <xuqiang@itiger.com>
2019-03-04 20:00:36 +0800

    [tigeropen][analysisdemo] refactor

--- tigeropen/examples/tinyquant/strategy_quote_trigger.py
+++ tigeropen/examples/tinyquant/strategy_quote_trigger.py
@@ -1,4 +1,4 @@
-import numpy as np
+from tigeropen.examples.tinyquant.compatibility import symbols
 
 
 class TickerTrendUtil:
@@ -30,25 +30,6 @@ class TickerTrendUtil:
             return False
 
 
-class EMAUtil:
-    def __init__(self, size):
-        self.buf = np.zeros(size)
-        self.maxsize = size
-        self.head = 0
-        self.sum = 0
-
-    def get_osc(self):
-        return self.sum / self.maxsize
-
-    def on_new_value(self, value):
-        tmp = self.buf[self.head]
-        self.buf[self.head] = value
-        self.sum += value - tmp
-        self.head += 1
-        if self.head >= self.maxsize:
-            self.head -= self.maxsize
-
-
 class Strategy(object):
     def __init__(self, push_client=None, trade_client=None, quote_client=None, context=None):
 
@@ -57,14 +38,9 @@ class Strategy(object):
         self.quote_client = quote_client
         self.context = context
 
-        # [subscribe symbols][necessary setting]
-        self.context.subscribe('AAPL')
-        # self.context.subscribe('01810')
-        # [strategy related][not necessary setting] local vars with different strategy
-        self.symbol_set = set(self.context.subscribed_symbols)
+        self.symbol_set = symbols('AAPL', 'BABA')
+
         self.tick_util = TickerTrendUtil()
-        self.short_min_ma = EMAUtil(10)
-        self.long_min_ma = EMAUtil(20)
 
     def on_ticker(self, symbol, items, hour_trading):
         """
@@ -72,11 +48,9 @@ class Strategy(object):
         """
         if hour_trading:
             return
-        print(symbol, items, hour_trading)
         latest_price = dict(items).get('latest_price')
         if symbol in self.symbol_set:
             if self.tick_util.on_latest_price(symbol, latest_price):
-                print('============= create order =============')
                 contract = self.context.contract_map.get(symbol)
                 self.trade_client.create_order(self.context.account, contract, 'BUY', 'MKT', 100, limit_price=latest_price)
 
