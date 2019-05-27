80fef9f365892c6f534cfd3a4847db88047c0379
gaoan <gaoan@tigerbrokers.com>
2019-05-27 15:10:28 +0800

    fix: fix trade and quote issue

--- tigeropen/examples/trade_client_demo.py
+++ tigeropen/examples/trade_client_demo.py
@@ -7,14 +7,15 @@ Created on 2018/9/20
 import logging
 import traceback
 
-from tigeropen.trade.domain.order import ORDER_STATUS
+from tigeropen.trade.domain.order import OrderStatus
 from tigeropen.trade.request.model import AccountsParams
 from tigeropen.common.response import TigerResponse
 from tigeropen.tiger_open_client import TigerOpenClient
 from tigeropen.trade.trade_client import TradeClient
 from tigeropen.quote.request import OpenApiRequest
 from tigeropen.examples.client_config import get_client_config
-from tigeropen.common.util.contract_utils import option_contract, future_contract
+# from tigeropen.common.util.contract_utils import stock_contract, option_contract, future_contract
+# from tigeropen.common.util.order_utils import limit_order
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -52,6 +53,10 @@ def get_account_apis():
     openapi_client.get_managed_accounts()
     # 获取订单
     openapi_client.get_orders()
+    # 获取未成交订单
+    # openapi_client.get_open_orders()
+    # 获取已成交订单
+    # openapi_client.get_filled_orders(start_time='2019-05-01', end_time='2019-05-21')
     # 获取持仓
     openapi_client.get_positions()
     # 获取资产
@@ -61,16 +66,23 @@ def get_account_apis():
 def trade_apis():
     account = client_config.account
     openapi_client = TradeClient(client_config, logger=logger)
+
     # stock
     contract = openapi_client.get_contracts('AAPL')[0]
+    # 或者本地构造合约对象
+    # contract = stock_contract(symbol='AAPL', currency='USD')
+
     # option
     # contract = option_contract(identifier='AAPL  190118P00160000')
     # future
-    # contract = future_contract('CHF', 'USD', '20190617', multiplier=125000)
-    # contract.exchange = 'GLOBEX'
-    order = openapi_client.create_order(account, contract, 'BUY', 'LMT', 100, limit_price=0.1)
-    order_id = order.order_id  # you can operate order via id too
+    # contract = future_contract('CHF', 'USD', '20190617', multiplier=125000, exchange='GLOBEX')
+
+    order = openapi_client.create_order(account, contract, 'BUY', 'LMT', 100, limit_price=5.0)
+    # 或者本地构造订单对象
+    # order = limit_order(account=account, contract=contract, action='BUY', quantity=100, limit_price=5.0)
     openapi_client.place_order(order)
+    order_id = order.order_id  # you can operate order via id too
+
     new_order = openapi_client.get_order(order_id=order.order_id)
     assert order.order_id == new_order.order_id
     openapi_client.modify_order(new_order, quantity=150)
@@ -78,7 +90,7 @@ def trade_apis():
     assert new_order.quantity == 150
     openapi_client.cancel_order(order_id=order_id)
     new_order = openapi_client.get_order(order_id=order_id)
-    assert new_order.status == ORDER_STATUS.CANCELLED or new_order.status == ORDER_STATUS.PENDING_CANCEL
+    assert new_order.status == OrderStatus.CANCELLED or new_order.status == OrderStatus.PENDING_CANCEL
 
 
 if __name__ == '__main__':
