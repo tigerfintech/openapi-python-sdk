6eea86ef5e0655cd2159811e36adb72bcdc5b6f3
Kai Su <sukai@tigerbrokers.com>
2019-06-21 20:06:00 +0800

    modified:   tigeropen/trade/response/__init__.py 	modified:   tigeropen/trade/response/contracts_response.py

--- tigeropen/trade/response/__init__.py
+++ tigeropen/trade/response/__init__.py
@@ -5,5 +5,6 @@ Created on 2018/10/31
 @author: gaoan
 """
 
-CONTRACT_FIELDS = set(['symbol', 'market', 'multiplier', 'sec_type', 'currency', 'local_symbol', 'origin_symbol',
-                       'expiry', 'strike', 'right', 'contract_id'])
+CONTRACT_FIELDS = {'symbol', 'market', 'multiplier', 'sec_type', 'currency', 'local_symbol', 'origin_symbol',
+                   'expiry', 'strike', 'right', 'contract_id', 'exchange'}
+
