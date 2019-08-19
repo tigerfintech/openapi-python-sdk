768a52983b563b2ed884fd6b7f97916eb6e4d5fb
Kai Su <sukai@tigerbrokers.com>
2019-08-19 16:46:37 +0800

    modified:   tigeropen/quote/response/quote_depth_entry_response.py

--- tigeropen/quote/response/quote_depth_entry_response.py
+++ tigeropen/quote/response/quote_depth_entry_response.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
 from tigeropen.common.response import TigerResponse
 from tigeropen.common.util.string_utils import get_string
 
@@ -7,40 +6,46 @@ from tigeropen.common.util.string_utils import get_string
 class DepthEntryResponse(TigerResponse):
     def __init__(self):
         super(DepthEntryResponse, self).__init__()
-        self.depth_entry = []
+        self.depth_entry = dict()
         self._is_success = None
 
     def parse_response_content(self, response_content):
         """
-        :return: pandas.DataFrame
-            symbol    ask_price ask_volume ask_count   bid_price bid_volume bid_count
-            SYMBOL1   卖1价      卖1股数     卖1订单数    买1价     买1股数     买1订单数
-            SYMBOL1   卖2价      卖2股数     卖2订单数    买2价     买2股数     买2订单数
-               .
-               .
-            SYMBOL1   卖10价     卖10股数    卖10订单数   买10价    买10股数    买10订单数
-
-
-            SYMBOL2   卖1价      卖1股数     卖1订单数    买1价     买1股数     买1订单数
-            SYMBOL2   卖2价      卖2股数     卖2订单数    买2价     买2股数     买2订单数
-               .
-               .
-            SYMBOL2   卖10价     卖10股数    卖10订单数  买10价    买10股数     买10订单数
+        :return:
+        { '02833': { 'asks': [
+                        { 'count': 1, 'price': 27, 'volume': 3100 },
+                        { 'count': 0, 'price': 27.05, 'volume': 0 },
+                        .
+                        .
+                        { 'count': 1, 'price': 27.45, 'volume': 300 }],
+                    'bids': [
+                        { 'count': 1, 'price': 26.85, 'volume': 1000 },
+                        { 'count': 0, 'price': 26.8, 'volume': 0 },
+                        .
+                        .
+                        { 'count': 0, 'price': 26.4, 'volume': 0 } ],
+                    'symbol': '02833' },
+        '02828': { 'asks': [
+                        { 'count': 3, 'price': 103.3, 'volume': 172200 },
+                        .
+                        .
+                        { 'count': 2, 'price': 104.2, 'volume': 5200 } ],
+                    'bids': [
+                        { 'count': 3, 'price': 103.2, 'volume': 166200 },
+                        .
+                        .
+                        { 'count': 1, 'price': 102.3, 'volume': 88800 } ],
+                    'symbol': '02828' }
+        }
         """
         response = super(DepthEntryResponse, self).parse_response_content(response_content)
         if 'is_success' in response:
             self._is_success = response['is_success']
 
         if self.data and isinstance(self.data, list):
-            result = list()
             for item in self.data:
                 symbol = get_string(item.get('symbol'))
-                asks = pd.DataFrame(item.get('asks', []))[['count', 'volume', 'price']].add_prefix('ask_')
-                bids = pd.DataFrame(item.get('bids', []))[['price', 'volume', 'count']].add_prefix('bid_')
-                merged_data = pd.concat([asks, bids], axis=1)
-                merged_data.insert(loc=0, column='symbol', value=symbol)
-                result.append(merged_data)
-            self.depth_entry = pd.concat(result)
+                self.depth_entry[symbol] = item
             return self.depth_entry
 
 
