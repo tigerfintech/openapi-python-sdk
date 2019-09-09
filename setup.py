b555341b545b9fac27de4435ee428daff1070458
Kai Su <sukai@tigerbrokers.com>
2019-09-09 10:46:41 +0800

    modified:   setup.py 	modified:   tigeropen/quote/response/quote_order_book_response.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ Created on 2018/9/16
 from os import path
 from setuptools import find_packages, setup
 
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt'), encoding='utf-8') as f:
     install_requires = f.read()
 
 setup(
