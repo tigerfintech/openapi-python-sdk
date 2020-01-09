8743ce03401f13ee760cc3708ffef7b1cb7d87a0
Kai Su <sukai@itiger.com>
2020-01-09 17:59:11 +0800

    fix python2.7 encoding

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Created on 2018/9/16
 from os import path
 from setuptools import find_packages, setup
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt'), encoding='utf-8') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt')) as f:
     install_requires = f.read()
 
 setup(
