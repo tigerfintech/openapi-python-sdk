19d97a4226487df1980a9ca91010bc3a6a14a873
Su <sukai@itiger.com>
2021-11-17 16:39:55 +0800

    delay quote api

--- tigeropen/quote/response/quote_delay_briefs_response.py
+++ tigeropen/quote/response/quote_delay_briefs_response.py
@@ -9,11 +9,7 @@ import pandas as pd
 
 from tigeropen.common.response import TigerResponse
 
-COLUMNS = ['symbol', 'ask_price', 'ask_size', 'bid_price', 'bid_size', 'pre_close', 'latest_price', 'latest_time',
-           'volume', 'open', 'high', 'low', 'status']
-BRIEF_FIELD_MAPPINGS = {'askPrice': 'ask_price', 'askSize': 'ask_size', 'bidPrice': 'bid_price', 'bidSize': 'bid_size',
-                        'latestPrice': 'latest_price', 'preClose': 'pre_close', 'latestTime': 'latest_time',
-                        'avgPrice': 'avg_price', 'adjPreClose': 'adj_pre_close'}
+from tigeropen.common.util import string_utils
 
 
 class DelayBriefsResponse(TigerResponse):
@@ -26,6 +22,7 @@ class DelayBriefsResponse(TigerResponse):
         response = super(DelayBriefsResponse, self).parse_response_content(response_content)
         if 'is_success' in response:
             self._is_success = response['is_success']
-
         if self.data and isinstance(self.data, list):
-            self.briefs = pd.DataFrame(self.data).rename(columns=BRIEF_FIELD_MAPPINGS)
+            df = pd.DataFrame(self.data)
+            field_mapping = {item: string_utils.camel_to_underline(item) for item in df.columns}
+            self.briefs = df.rename(columns=field_mapping)
