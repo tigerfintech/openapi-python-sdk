1c05372755a9d5648aaaebc5fc2912640633feed
Su <sukai@itiger.com>
2021-11-17 11:27:57 +0800

    fix grab_quote_permission biz_model None

--- tigeropen/quote/request/__init__.py
+++ tigeropen/quote/request/__init__.py
@@ -26,7 +26,7 @@ class OpenApiRequest:
     def get_params(self):
         params = dict()
         params[P_METHOD] = self._method
-        params[P_VERSION] = getattr(self.biz_model, P_VERSION)
+        params[P_VERSION] = getattr(self.biz_model, P_VERSION, None)
 
         if self.biz_model:
             params[P_BIZ_CONTENT] = json.dumps(obj=self.biz_model.to_openapi_dict(), ensure_ascii=False, sort_keys=True,
