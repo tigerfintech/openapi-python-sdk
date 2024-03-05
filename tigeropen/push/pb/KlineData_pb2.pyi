7f7862b29666b74c47b2a3eaf94e00aa54c19959
Su <sukai@itiger.com>
2024-03-05 20:01:55 +0800

    mod klinedata

--- tigeropen/push/pb/KlineData_pb2.pyi
+++ tigeropen/push/pb/KlineData_pb2.pyi
@@ -5,10 +5,9 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class KlineData(_message.Message):
-    __slots__ = ["amount", "avg", "barTime", "close", "count", "high", "low", "open", "serverTimestamp", "symbol", "volume"]
+    __slots__ = ["amount", "avg", "close", "count", "high", "low", "open", "serverTimestamp", "symbol", "time", "volume"]
     AMOUNT_FIELD_NUMBER: _ClassVar[int]
     AVG_FIELD_NUMBER: _ClassVar[int]
-    BARTIME_FIELD_NUMBER: _ClassVar[int]
     CLOSE_FIELD_NUMBER: _ClassVar[int]
     COUNT_FIELD_NUMBER: _ClassVar[int]
     HIGH_FIELD_NUMBER: _ClassVar[int]
@@ -16,10 +15,10 @@ class KlineData(_message.Message):
     OPEN_FIELD_NUMBER: _ClassVar[int]
     SERVERTIMESTAMP_FIELD_NUMBER: _ClassVar[int]
     SYMBOL_FIELD_NUMBER: _ClassVar[int]
+    TIME_FIELD_NUMBER: _ClassVar[int]
     VOLUME_FIELD_NUMBER: _ClassVar[int]
     amount: float
     avg: float
-    barTime: int
     close: float
     count: int
     high: float
@@ -27,5 +26,6 @@ class KlineData(_message.Message):
     open: float
     serverTimestamp: int
     symbol: str
+    time: int
     volume: int
-    def __init__(self, barTime: _Optional[int] = ..., open: _Optional[float] = ..., high: _Optional[float] = ..., low: _Optional[float] = ..., close: _Optional[float] = ..., avg: _Optional[float] = ..., volume: _Optional[int] = ..., count: _Optional[int] = ..., symbol: _Optional[str] = ..., amount: _Optional[float] = ..., serverTimestamp: _Optional[int] = ...) -> None: ...
+    def __init__(self, time: _Optional[int] = ..., open: _Optional[float] = ..., high: _Optional[float] = ..., low: _Optional[float] = ..., close: _Optional[float] = ..., avg: _Optional[float] = ..., volume: _Optional[int] = ..., count: _Optional[int] = ..., symbol: _Optional[str] = ..., amount: _Optional[float] = ..., serverTimestamp: _Optional[int] = ...) -> None: ...
