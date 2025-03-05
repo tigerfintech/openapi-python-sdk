3eea5d7d8d3a5d40f58bc33224b2fdd40fbad34b
Su <sukai@itiger.com>
2025-03-05 17:09:50 +0800

    position query asset_quote_type

--- tigeropen/common/consts/__init__.py
+++ tigeropen/common/consts/__init__.py
@@ -246,3 +246,11 @@ class OptionRankingIndicator(Enum):
     Volume = "volume"
     Amount = "amount"
     OpenInt = "openInt"
+
+class AssetQuoteType(Enum):
+    # Includes pre-market, intra-day, and after-hours trading data. For night session, the closing price of the previous after-hours trading is used for calculation.
+    ETH = "ETH"
+    # Only intra-day trading data. For pre-market, after-hours, and night session, the intra-day closing price is used for calculation.
+    RTH = "RTH"
+    # Includes night session trading data. For night session, the night session trading data is used for calculation.
+    OVERNIGHT = "OVERNIGHT"
